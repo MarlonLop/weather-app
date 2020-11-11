f712b70da092888b2c6a0a5ff7737d3aed05515c
Marlon Lopez <marlonlop.cs@gmail.com>
2020-11-11 13:15:16 -0700

    display on client weather result

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,23 @@ console.log('Client side JS file is loaded');
 
 const weatherForm = document.querySelector('form');
 const getWeather = document.querySelector('input');
+const message1 = document.querySelector('#message-1');
+const message2 = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const location = getWeather.value
+    message1.textContent ='Fetching weather...';
+    message2.textContent ='';
 
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error){
-                console.log(data.error);
+                message1.textContent = data.error;
             } else {
-                console.log(data.location, data.forecast);
+                message1.textContent = data.location;
+                message2.textContent = data.forecast;
             }
         });
     });
