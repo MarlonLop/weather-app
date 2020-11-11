796a4e37dacbd407ab72a3e646875e134b3365b9
Marlon Lopez <marlonlop.cs@gmail.com>
2020-11-11 12:59:54 -0700

    weather input form and event listener with fetch added

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,20 @@
 console.log('Client side JS file is loaded');
 
-fetch('http://localhost:3000/weather?address=').then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            console.log(data.error);
-        } else {
-            console.log(data.location, data.forecast);
-        }
+const weatherForm = document.querySelector('form');
+const getWeather = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const location = getWeather.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                console.log(data.error);
+            } else {
+                console.log(data.location, data.forecast);
+            }
+        });
     });
 });
