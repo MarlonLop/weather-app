96e3b1a3a070ae8e5e1914d2ff2eb467fc0c0b07
Marlon Lopez <marlonlop.cs@gmail.com>
2020-11-11 21:45:39 -0700

    gettng ready for heroku deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -84,6 +85,6 @@ app.get('*', (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('server up on port 3000');
+app.listen(PORT, () => {
+    console.log('server up on port ' + PORT);
 });
